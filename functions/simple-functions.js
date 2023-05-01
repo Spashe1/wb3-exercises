7fb281725a9c39bb52b09df99b6b4eba9945bb18
Sephora Pashe <Spashe@appdev.yearup.org>
2023-05-01 10:36:51 -0400

    functions file

---
+++ functions/simple-functions.js
@@ -0,0 +1,16 @@
+function favorateThings(){
+    console.log("My name is Sephora");
+    console.log("Favorite movie is The note book");
+    console.log("favorite band is...");
+}
+
+function whyImHere(){
+    console.log('Im here because i want to learn to code');
+}
+function favoritePlace(){
+console.log('My favorite place is Ethiopia, because it reminds me of my childhood');
+}
+
+favorateThings();
+whyImHere();
+favorateThings();
